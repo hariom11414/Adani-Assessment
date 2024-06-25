25e248d703be797c81c5002850310dabcf8dc94b
hariom936 <hariom.verma@kellton.com>
2024-06-25 17:51:57 +0530

    Adani-assessment

---
+++ src/entity/Users.ts
@@ -0,0 +1,36 @@
+import { IsDate } from "class-validator";
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm"
+
+@Entity()
+export class Users {
+    @PrimaryGeneratedColumn({ type: 'int' })
+    id: number;
+
+    @Column({ nullable: true })
+    first_name: string;
+
+    @Column({ nullable: true })
+    last_name: string;
+
+    @Column({ nullable: true })
+    email: string;
+
+    @Column({ type: 'bigint' })  // Change here
+    phone: number;
+   
+
+    @Column({ type: 'bigint' })  // Change here
+    password: number;
+
+    
+
+    @Column({ type: 'timestamp'})
+    birthday: Date;
+
+    @CreateDateColumn({ type: 'timestamp' })
+    createdAt: Date;
+
+    @UpdateDateColumn({ type: 'timestamp' })
+    updatedAt: Date;
+  static id: any;
+}
